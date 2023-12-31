80809e2007cb74cf27c7ed5e298d8f6f9aacb2a2
zch <68337357+flateon@users.noreply.github.com>
2023-12-31 13:28:31 +0800

    fix KWS bugs

--- terminal.py
+++ terminal.py
@@ -1,5 +1,3 @@
-import time
-
 import pyaudio
 from rich.progress import Progress, BarColumn, TextColumn
 
@@ -24,7 +22,10 @@ if __name__ == '__main__':
             audio_input_vol = StreamWithVolumeDisplay(audio_input,
                                                       lambda v: progress.update(vol, visible=True, completed=v))
 
-            kws = KWS.KeywordSpotter()
+            kws = KWS.KeywordSpotter('./Records/xiaoyuan')
+            asr = ASR(TerminalASRCallback(logger=printer.get_live_print('User')), audio_input=audio_input_vol)
+            tts = TTS(TerminalTTSCallback(audio_output), sample_rate=24000,
+                      model={'zh': 'sambert-zhiwei-v1', 'en': 'sambert-cindy-v1'})
 
             while True:
                 kws.detect(audio_input_vol)
@@ -32,15 +33,11 @@ if __name__ == '__main__':
                 tts.say('我在，有什么我可以帮助你的吗？')
                 printer.get_live_print('Bot')('我在，有什么我可以帮助你的吗？', add_history=True)
 
-                asr = ASR(TerminalASRCallback(logger=printer.get_live_print('User')), audio_input=audio_input_vol)
-                tts = TTS(TerminalTTSCallback(audio_output), sample_rate=24000,
-                          model={'zh': 'sambert-zhiwei-v1', 'en': 'sambert-cindy-v1'})
                 llm = LLMPlugin(TerminalCallback(tts, logger=printer.get_live_print('Bot')))
-
                 while True:
                     user_msg = asr.start(idle_timeout=20)
-                    # asr timeout
                     if user_msg is None:
+                        # asr idle timeout
                         printer.get_live_print('Bot')('如有需要，请随时召唤我。祝您有美好的一天！再见！', add_history=True)
                         tts.say('如有需要，请随时召唤我。祝您有美好的一天！再见！')
                         break
